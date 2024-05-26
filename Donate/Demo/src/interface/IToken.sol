a8e0957da32a2571fe229907bb8b3bac50b15e4c
DawnBlackA <127305377+DawnBlackA@users.noreply.github.com>
2024-05-26 18:59:50 +0800

    Delete Donate/Demo directory

--- Donate/Demo/src/interface/IToken.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.13;
-
-interface Itoken {
-    function getPriorVotes(address amount,uint blocknumber) external view returns(uint);
-}
